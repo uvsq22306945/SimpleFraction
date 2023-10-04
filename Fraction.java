99765f29d66830ca46826ced625fb998e9aa6929
uvsq22306945 <a.mohamedgalab@gmail.com>
2023-10-04 21:42:26 +0200

    first commit

---
+++ Fraction.java
@@ -0,0 +1,5 @@
+package td1.SimpleFraction;
+
+public class Fraction {
+    
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package td1.SimpleFraction;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, world");
+    }
+}
